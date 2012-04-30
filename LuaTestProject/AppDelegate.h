1fd87e23c9bb58fbafd8c639f60f3af885d25dc7
Kevin Jenkins <kevinj04@gmail.com>
2012-04-30 15:41:12 -0400

    Initial Commit

---
+++ LuaTestProject/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  LuaTestProject
+//
+//  Created by Kevin Jenkins on 4/30/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ LuaTestProject/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  LuaTestProject
+//
+//  Created by Kevin Jenkins on 4/30/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
